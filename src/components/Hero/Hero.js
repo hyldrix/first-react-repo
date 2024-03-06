af61431bdb9e26484595ad6d7e15e39fec892b63
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-03-06 21:25:59 +0200

    Added: PageTitle module for easy generation of headers

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,13 @@
 import styles from './Hero.module.scss'
+import PageTitle from '../PageTitle/PageTitle.js';
 const Hero = () => {
-    return (
-      <div className={styles.hero}>
-        <h1 className={styles.title}>My first React App</h1>
-        <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
-      </div>
-    );
-  };
+  return (
 
-  export default Hero;
+    <div className={styles.hero}>
+      <PageTitle>My first React App</PageTitle>
+      <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
+    </div>
+  );
+};
+
+export default Hero;
