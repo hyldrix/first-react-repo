5a17dbec37b1a3652c3f80e83133fafd812e13be
hyldrix <117600898+hyldrix@users.noreply.github.com>
2025-03-07 16:31:55 +0100

    Add failsafe if str2 is not provided

--- src/utils/strContains.js
+++ src/utils/strContains.js
@@ -1,4 +1,11 @@
 
-export const compareStrings =  (str1, str2) => {
-    return str1.toLowerCase().includes(str2.toLowerCase())
+export const compareStrings = (str1, str2) => {
+
+if (!str2){
+    str2 = {
+        searchString: ''
+    } 
 }
+
+    return str1.toLowerCase().includes(str2.searchString.toLowerCase());
+};
